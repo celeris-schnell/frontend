70d2d4a1325314b432b081eeb03d3fc440c24812
Amal M Raju <amalraju179@gmail.com>
2025-04-04 19:28:09 +0530

    feat: add qr code generator

---
+++ app/src/main/java/in/dunder/celeris/utils/QRCodeUtils.java
@@ -0,0 +1,30 @@
+package in.dunder.celeris.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.common.BitMatrix;
+
+public class QRCodeUtils {
+    public static Bitmap generateQRCode(String merchantId) {
+        QRCodeWriter writer = new QRCodeWriter();
+        try {
+            BitMatrix bitMatrix = writer.encode(merchantId, BarcodeFormat.QR_CODE, 512, 512);
+            int width = bitMatrix.getWidth();
+            int height = bitMatrix.getHeight();
+            Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    bmp.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
+                }
+            }
+            return bmp;
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
