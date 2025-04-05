864e66b7a0bbc8cf6e58eb7439a7985da3308e56
Aayush Shukla <oreus.aks@gmail.com>
2025-04-05 16:31:25 +0530

    feat: get user object for abstraction and further use

--- app/src/main/java/in/dunder/celeris/db/AuthDatabaseHelper.java
+++ app/src/main/java/in/dunder/celeris/db/AuthDatabaseHelper.java
@@ -1,9 +1,12 @@
 package in.dunder.celeris.db;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import in.dunder.celeris.models.User;
 
 public class AuthDatabaseHelper extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
@@ -20,7 +23,8 @@ public class AuthDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS users");
+        onCreate(db);
     }
 
     public static boolean checkDatabaseExists(Context context) {
@@ -28,7 +32,7 @@ public class AuthDatabaseHelper extends SQLiteOpenHelper {
         String DB_FULL_PATH = context.getDatabasePath("authdb").getPath();
         try {
             checkDB = SQLiteDatabase.openDatabase(DB_FULL_PATH, null, SQLiteDatabase.OPEN_READONLY);
-        } catch (Exception e) {
+        } catch (Exception ignored) {
         }
 
         if (checkDB != null) {
@@ -36,13 +40,6 @@ public class AuthDatabaseHelper extends SQLiteOpenHelper {
         }
 
         return checkDB != null;
-    }
-
-    public void insertUser(int id, String name, int balance, String email, String phoneNumber) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        db.execSQL("INSERT INTO users (id, name, balance, email, phoneNumber) VALUES (?, ?, ?, ?, ?)",
-                new Object[]{id, name, balance, email, phoneNumber});
-        db.close();
     }
 
     public void saveUser(int id, String name, int balance, String email, String phoneNumber) {
@@ -55,7 +52,7 @@ public class AuthDatabaseHelper extends SQLiteOpenHelper {
     public boolean isUserLoggedIn() {
         SQLiteDatabase db = this.getReadableDatabase();
         boolean loggedIn = false;
-        try (Cursor cursor = db.rawQuery("SELECT COUNT(*) FROM users", null)) {
+        try (Cursor cursor = db.rawQuery("SELECT COUNT(*) FROM users LIMIT 1", null)) {
             if (cursor.moveToFirst()) {
                 int count = cursor.getInt(0);
                 loggedIn = count > 0;
@@ -63,5 +60,22 @@ public class AuthDatabaseHelper extends SQLiteOpenHelper {
         }
         db.close();
         return loggedIn;
+    }
+
+    public User getUser() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        User user = null;
+        try (Cursor cursor = db.rawQuery("SELECT * FROM users", null)) {
+            if (cursor.moveToFirst()) {
+                @SuppressLint("Range") int id = cursor.getInt(cursor.getColumnIndex("id"));
+                @SuppressLint("Range") String name = cursor.getString(cursor.getColumnIndex("name"));
+                @SuppressLint("Range") int balance = cursor.getInt(cursor.getColumnIndex("balance"));
+                @SuppressLint("Range") String email = cursor.getString(cursor.getColumnIndex("email"));
+                @SuppressLint("Range") String phoneNumber = cursor.getString(cursor.getColumnIndex("phoneNumber"));
+                user = new User(id, name, balance, email, phoneNumber);
+            }
+        }
+        db.close();
+        return user;
     }
 }
