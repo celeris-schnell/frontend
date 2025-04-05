b1d9bc4b7a0aed14bf08c27a9b1b7e872e02e81f
Aayush Shukla <oreus.aks@gmail.com>
2025-04-05 16:47:19 +0530

    feat: network monitor

---
+++ app/src/main/java/in/dunder/celeris/utils/NetworkMonitor.kt
@@ -0,0 +1,53 @@
+package `in`.dunder.celeris.utils
+
+import android.content.Context
+import android.net.*
+
+class NetworkMonitor(
+    context: Context,
+    private val onNetworkChange: (isOnline: Boolean, justCameOnline: Boolean) -> Unit
+) {
+    private val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    private var wasOnline = false
+
+    private val networkCallback = object : ConnectivityManager.NetworkCallback() {
+        override fun onAvailable(network: Network) {
+            val isOnline = isCurrentlyOnline()
+            if (!wasOnline && isOnline) {
+                onNetworkChange(true, true) // just came online
+            } else {
+                onNetworkChange(true, false)
+            }
+            wasOnline = true
+        }
+
+        override fun onLost(network: Network) {
+            onNetworkChange(false, false)
+            wasOnline = false
+        }
+    }
+
+    fun start() {
+        val request = NetworkRequest.Builder()
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            .build()
+        connectivityManager.registerNetworkCallback(request, networkCallback)
+        wasOnline = isCurrentlyOnline()
+    }
+
+    fun stop() {
+        try {
+            connectivityManager.unregisterNetworkCallback(networkCallback)
+        } catch (_: Exception) {
+        }
+    }
+
+    fun isCurrentlyOnline(): Boolean {
+        val network = connectivityManager.activeNetwork ?: return false
+        val capabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
+        return capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
+                capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+    }
+}
