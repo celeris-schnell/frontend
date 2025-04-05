2c71e7106474e41c8c337094d9ab97517e370735
Amal M Raju <amalraju179@gmail.com>
2025-04-05 12:52:00 +0530

    chore: scanner running

--- app/src/main/java/in/dunder/celeris/frontend/MainActivity.kt
+++ app/src/main/java/in/dunder/celeris/frontend/MainActivity.kt
@@ -1,7 +1,6 @@
 package `in`.dunder.celeris.frontend
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -10,19 +9,19 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        // Handle window insets for system bars (status bar, navigation bar)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        // Load the ProfilePage fragment
+        // Load the ScanPayFragment into the fragment container
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, ProfilePage())
+                .replace(R.id.fragment_container, ScanPayFragment())
                 .commit()
         }
     }
